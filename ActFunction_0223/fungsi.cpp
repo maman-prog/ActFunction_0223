17fa7302ac2a72f7987bb747eab205b449b7a54d
maman-prog <ingratarmanrizky19@gmail.com>
2026-03-02 21:47:24 +0700

    step kedua

--- ActFunction_0223/fungsi.cpp
+++ ActFunction_0223/fungsi.cpp
@@ -1,3 +1,12 @@
 //libary
 #include <iostream>
 using namespace std;
+//deklarasi variabel
+int p, l;
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "Masukkan Panjang :";
+    cin >> p;
+    cout << "Masukkan Lebar :";
+    cin >> l;
+}
